801e465bead2a27b4d61bf89da67f059fc631bfb
Luis Lopez <darkdestruktion@gmail.com>
2021-05-08 15:34:03 -0500

    Add project files.

---
+++ ProductorConsumidorSemaforo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace ProductorConsumidorSemaforo
+{
+    class Program
+    {
+        Semaphore producer = new Semaphore(1, 1);
+        Semaphore consumer = new Semaphore(0, 1);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
