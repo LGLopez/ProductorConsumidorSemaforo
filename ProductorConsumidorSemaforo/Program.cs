9ae3b826b83a57abedb2236e0dbe9098083418b6
LUIS GERARDO LOPEZ VAZQUEZ <LuisG.Lopez@alumno.udg.mx>
2021-05-08 19:59:48 -0500

    Solo uno entra al buffer

--- ProductorConsumidorSemaforo/Program.cs
+++ ProductorConsumidorSemaforo/Program.cs
@@ -14,21 +14,30 @@ namespace ProductorConsumidorSemaforo
             Random rand = new Random();
             bool isDone = false;
             int i = 0, consumerCounter = 0;
+            bool isProducerIn = true;
 
             void produce()
             {
                 int count = 0;
-                for (int i = 0; count < 20; i++, count++)
+                for (int i = 0; count < 20; )
                 {
-                    producer.WaitOne();
-                    if (i >= 20)
-                        i = 0;
-                    
-                    if (buffer[i] == 0) 
+                    if (isProducerIn)
                     {
-                        int num = rand.Next(1, 99);
-                        buffer[i] = num;
-                        Console.WriteLine("El productor agrego el {0} al buffer en la posicion {1}", num, i);
+                        producer.WaitOne();
+                    
+                        if (i >= 20)
+                            i = 0;
+
+                        if (buffer[i] == 0)
+                        {
+                            int num = rand.Next(1, 99);
+                            buffer[i] = num;
+                            Console.WriteLine("El productor agrego el {0} al buffer en la posicion {1}", num, i);
+                        }
+                        i++;
+                        count++;
+                        isProducerIn = false;
+                        Console.WriteLine("El productor esta dormido");
                     }
                 }
                 producer.WaitOne();
@@ -39,21 +48,25 @@ namespace ProductorConsumidorSemaforo
             {
                 while (!isDone)
                 {
-                    consumer.WaitOne();
-                    if (consumerCounter >= 20)
-                        consumerCounter = 0;
-
-                    if (buffer[consumerCounter] != 0)
+                    if (!isProducerIn)
                     {
-                        int numTaken = buffer[consumerCounter];
-                        buffer[consumerCounter] = 0;
-                        Console.WriteLine("El consumidor saco el valor {0} de la posicion {1}", numTaken, consumerCounter);
-                    }
-                    
-                    consumerCounter++;
-                    if (consumerCounter >= 20)
-                        consumerCounter = 0;
+                       consumer.WaitOne();
+                        if (consumerCounter >= 20)
+                            consumerCounter = 0;
 
+                        if (buffer[consumerCounter] != 0)
+                        {
+                            int numTaken = buffer[consumerCounter];
+                            buffer[consumerCounter] = 0;
+                            Console.WriteLine("El consumidor saco el valor {0} de la posicion {1}", numTaken, consumerCounter);
+                        }
+
+                        consumerCounter++;
+                        if (consumerCounter >= 20)
+                            consumerCounter = 0;
+                        isProducerIn = true;
+                        Console.WriteLine("El consumidor esta dormido");
+                    }
                 }
             }
 
@@ -61,9 +74,10 @@ namespace ProductorConsumidorSemaforo
             {
                 do
                 {
-                    Thread.Sleep(100);
+                    Thread.Sleep(rand.Next(1, 9) * 100);
                     if (buffer[i] == 0)
                         producer.Release();
+                    Thread.Sleep(rand.Next(1, 9) * 100);
                     if (buffer[consumerCounter] != 0)
                         consumer.Release();
 
